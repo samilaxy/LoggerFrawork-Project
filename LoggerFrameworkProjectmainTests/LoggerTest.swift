cdc3f8b4c07006c60de483bd668ff08cd94c7f8c
Noye Samuel <samuelnoye7@gmail.com>
2022-12-22 12:02:58 +0000

    remove env

--- LoggerFrameworkProjectmainTests/LoggerTest.swift
+++ LoggerFrameworkProjectmainTests/LoggerTest.swift
@@ -12,7 +12,6 @@ final class LoggerTest: XCTestCase {
     //create SUT
     
     var sut: Logger!
-    var env = Logger.Environment.self
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
